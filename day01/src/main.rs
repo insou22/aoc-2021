59e5d30cc0036a72aed1320570b1bafc017338b3
insou22 <zac.kologlu@gmail.com>
2021-12-01 17:22:41 +1100

    day01: rustfmt

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -13,36 +13,30 @@ fn main() {
                 .collect::<Vec<_>>();
 
             let count = match part {
-                "1" => {
-                    input.windows(2)
-                        .filter(|window| match window {
-                            &[a, b] => a < b,
-                            _       => unreachable!(),
-                        })
-                        .count()
-                }
-                "2" => {
-                    input.windows(4)
-                        .filter(|window| match window {
-                            &[a, b, c, d] => (a + b + c) < (b + c + d),
-                            _       => unreachable!(),
-                        })
-                        .count()
-                }
+                "1" => input
+                    .windows(2)
+                    .filter(|window| match window {
+                        &[a, b] => a < b,
+                        _ => unreachable!(),
+                    })
+                    .count(),
+                "2" => input
+                    .windows(4)
+                    .filter(|window| match window {
+                        &[a, b, c, d] => (a + b + c) < (b + c + d),
+                        _ => unreachable!(),
+                    })
+                    .count(),
                 _ => unreachable!(),
             };
 
             println!("{}", count);
         }
         _ => {
-            let program = std::env::args()
-                .next()
-                .unwrap_or("<program>".to_string());
+            let program = std::env::args().next().unwrap_or("<program>".to_string());
 
             eprintln!("Usage: {} <1|2>", program);
             return;
         }
     }
-
-    
 }
